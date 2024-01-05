27851f561419d37abe19893521a466a8c47b7612
Yuri Cunha <isyuricunha@duck.com>
2024-01-05 15:39:26 -0300

    dot all

---
+++ generate.py
@@ -0,0 +1,13 @@
+import requests, json, random
+
+f = open("./README.md", "w")
+pokemon_id = random.randint(1, 151)
+res = requests.get(f'https://pokeapi.co/api/v2/pokemon/{pokemon_id}')
+result = json.loads(res.text)
+f.write(f'''<p align="center">
+    <img src="{result['sprites']['front_default']}" width="150" height="150">
+</p>
+<h3 align="center"> <b>{result['name'].title()}</b></h3>
+<h3 align="center">Have a terrifying day! (contains irony)</h3>
+''')
+f.close()
